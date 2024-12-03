8276150f4a4d53aa025f623b3122c80edc6fa5e6
Dorian <117446752+DorianTrazos@users.noreply.github.com>
2024-12-03 14:32:26 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Button from './components/button/Button';
+import Title from './components/title/Title';
+
+
+const App = () => {
+	return <>
+		<Button numberA={3} numberB ={8} />
+		<Title/>
+	</> 
+};
+
+export default App;
---
+++ src/components/title/Title.jsx
@@ -0,0 +1,5 @@
+const Title = () => {
+  return <h2>Soy un título</h2>
+}
+
+export default Title
